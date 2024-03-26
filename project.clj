4d369ec2e7146707e58711a436b6cae4073c45ff
Gian12315 <giancarlo.cytro@gmail.com>
2024-03-26 15:50:23 -0600

    Updated dependencies

--- project.clj
+++ project.clj
@@ -2,18 +2,23 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
                  [cheshire "5.12.0"]
-                 [compojure "1.6.1"]
+                 [compojure "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.5.1"]
+                 [ring-logger "1.1.1"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [com.github.seancorfield/honeysql "2.5.1103"]
                  [com.github.seancorfield/next.jdbc "1.3.909"]
+                 [org.slf4j/slf4j-simple "2.0.12"]
                  [org.xerial/sqlite-jdbc "3.45.1.0"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:init topicos.handler/setup-app
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
+  :ring {:init topicos.database/create-table
          :handler topicos.handler/app
          :port 3000}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]
+                        [org.clojure/test.check "0.9.0"]]}})
