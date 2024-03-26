6d623256ae057eb14c99d1a565358c7c6823a0a4
Gian12315 <giancarlo.cytro@gmail.com>
2024-03-26 17:14:11 -0600

    metrics/insert expects JSON input and documented metrics routes
    It's more uniform to expect JSON in all routes

--- src/topicos/handler.clj
+++ src/topicos/handler.clj
@@ -7,7 +7,7 @@
    [compojure.route :as route]
    [ring.util.response :refer [response]]
    [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-   [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+   [ring.middleware.json :refer [wrap-json-body wrap-json-response wrap-json-params]]
    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
    [ring.util.response :as res]))
 
@@ -25,19 +25,19 @@
 
   (GET "/metrics/last" []
             (controller/metrics-last))
+    
+  (POST "/metrics/insert" {{:keys [humidity]} :body}
+    (logging/info "Received humidity value of:" humidity)
+    (controller/metrics-insert humidity))
 
-  (GET "/" []
-    {:data [1 2 3]})
-  
-  (POST "/metrics/insert" [humidity]
-      (logging/info "Received humidity value of:" humidity)
-      (controller/metrics-insert humidity))
+  (GET "/images/insert" [json]
+    (controller/images-insert))
 
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
       wrap-json-response
-      wrap-json-body
+      (wrap-json-body {:keywords? true})
       wrap-with-logger
       (wrap-defaults api-defaults)))
